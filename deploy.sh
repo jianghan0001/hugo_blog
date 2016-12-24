a60bf9349fc0b7ecc9ff39652beb5cdf807e7a5f
hongwei <377546584@qq.com>
2016-12-24 09:31:19 +0800

    修改博客

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 hugo
-echo 更新public文件夹完毕
+echo ----更新public文件夹完毕----
 git add .
-echo 添加
+echo ----添加----
 git commit -m "修改博客"
-echo 提交
+echo ----提交----
 git push
-echo 推送本地博客
+echo ----推送本地博客----
 cd public/
-echo 进入public文件夹
+echo ----进入public文件夹----
 git add .
-echo 添加
+echo ----添加----
 git commit -m "推送博客"
-echo 提交
+echo ----提交----
 git push
-echo 推送部署github
+echo ----推送部署github----
 cd -
